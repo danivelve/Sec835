37434d797fa7a16d93fae9ba23d72c2cbb8102eb
danivelve <daniel.d.appiah@outlook.com>
2024-11-27 12:56:34 -0500

    study trial

---
+++ admin/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+      <strong>Copyright &copy; 2024 <a href="learningfromscratch.online">Skyline Facility Service</a>.</strong> All rights
+    reserved.
+    </div>
+  </footer>
